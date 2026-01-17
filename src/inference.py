0a88079ecd72eae40146a2774ac7ea967454102d
Shivam Patil <64972223+shivampatil98@users.noreply.github.com>
2026-01-17 18:44:45 +0100

    inference

--- src/inference.py
+++ src/inference.py
@@ -10,20 +10,54 @@ class YOLOv11inference:
 
     config = load_config()
     self.conf_threshold = config['model']['conf_threshold']
-    self.image_extensions = config['model']['data']['image extensions']
+    self.image_extensions = config8['data']['image exte8sions']
 
-    def run_inference(self, image_dir: str, save_results: bool = True, results_dir: str = "results"):
-        image_dir_path = Path(image_dir)
-        if not image_dir_path.exists() or not image_dir_path.is_dir():
-            raise ValueError(f"Invalid image directory path: {image_dir}")
 
-        if save_results:
-            results_path = Path(results_dir)
-            results_path.mkdir(parents=True, exist_ok=True)
+def process_image(self, image_path):
+      results = self.model.predict(
+            source=image_path,
+            conf=self.conf_threshold,
+            device=self.device
+      )
 
-        for image_path in image_dir_path.glob("*.*"):
-            results = self.model(image_path)
-            if save_results:
-                results.save(save_dir=results_path)
+      return results
 
-        return "Inference completed successfully."
+    detection = []
+    class_counts = {}
+
+    for result in results:
+        for box in result.boxes:
+            cls_id = int(box.cls[0])
+            conf = float(box.conf[0])
+            bbox = box.xyxy[0].tolist()
+
+            detection.append({
+                'class_id': cls_id,
+                'confidence': conf,
+                'bbox': bbox,
+                #"bbox": box.xyxy[0].tolist()
+                'count': 1
+            })
+            class_counts[cls_id] = class_counts.get(cls_id, 0) + 1
+
+            for det in detection:
+                det['count'] = class_counts[det['class_id']]
+
+    return {
+        'image_path': str(image_path),
+        'detection': detection,
+        'total_objects': len(detection),
+        'unique_class': list(class_counts.keys()),
+        'class_counts': class_counts
+    }
+    
+    def process_directory(self, directory):
+        
+        metadata = []
+
+        patterns = [f"*{ext}" for ext in self.extensions]
+
+        image_paths = []
+
+        for pattern in patterns:
+            image_paths.extend(Path(directory).glob(pattern))
